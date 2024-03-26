5c1645c2814bb5e890b6871776283483c4ab1bc3
Oleg Voroshylov <olegvoroshilov123@gmail.com>
2024-03-26 15:04:18 +0200

    create auth db context

---
+++ CodeBlog.API/Data/AuthDbContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeBlog.API.Data;
+
+public class AuthDbContext : IdentityDbContext
+{
+    public AuthDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var readerRoleId = "849c2448-c1c6-4617-9b77-f68408a96d09";
+        var writerRoleId = "f432abbc-1aee-4bc3-884e-f58f1939b24d";
+
+        var roles = new List<IdentityRole>()
+        {
+            new IdentityRole()
+            {
+                Id = readerRoleId,
+                Name = "Reader",
+                NormalizedName = "Reader".ToUpper(),
+                ConcurrencyStamp= readerRoleId
+            },
+            new IdentityRole()
+            {
+                Id = writerRoleId,
+                Name = "Writer",
+                NormalizedName = "Writer".ToUpper(),
+                ConcurrencyStamp= writerRoleId
+            }
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+
+        var adminId = "4cb2a21b-7e8c-482c-9c5d-3957df0c63b2";
+
+        var admin = new IdentityUser()
+        {
+            Id = adminId,
+            UserName = "Oleg123",
+            Email = "olegvoroshilov123@gmail.com",
+            NormalizedEmail = "olegvoroshilov123@gmail.com".ToUpper(),
+            NormalizedUserName = "Oleg123".ToUpper()
+        };
+
+        admin.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(admin, "Admin123");
+
+
+        builder.Entity<IdentityUser>().HasData(admin);
+
+        var adminRoles = new List<IdentityUserRole<string>>()
+        {
+            new()
+            {
+                UserId = adminId,
+                RoleId = readerRoleId
+            },
+            new()
+            {
+                UserId = adminId,
+                RoleId = writerRoleId
+            }
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+    }
+}
--- CodeBlog.API/Program.cs
+++ CodeBlog.API/Program.cs
@@ -2,8 +2,12 @@ using CodeBlog.API.Contracts.Profiles;
 using CodeBlog.API.Data;
 using CodeBlog.API.Repositories.Implementation;
 using CodeBlog.API.Repositories.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,12 +22,49 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("CodeBlogConnectionString"));
 });
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CodeBlogConnectionString"));
+});
+
 builder.Services.AddAutoMapper(typeof(CategoryProfile));
 builder.Services.AddAutoMapper(typeof(BlogPostProfile));
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 builder.Services.AddScoped<IImageRepository, ImageRepository>();
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("CodeBlogs")
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            AuthenticationType = "Jwt",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
 var app = builder.Build();
 
@@ -42,6 +83,8 @@ app.UseCors(options =>
     options.AllowAnyMethod();
     options.AllowAnyOrigin();
 });
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
