de1990a185af1e9d37f679ac0f382cb97eaccd74
Oleg Voroshylov <olegvoroshilov123@gmail.com>
2024-03-29 00:36:20 +0200

    add extention for services

---
+++ CodeBlog.API/Extensions/ServicesExtension.cs
@@ -0,0 +1,73 @@
+﻿using CodeBlog.API.Contracts.Profiles;
+using CodeBlog.API.Data;
+using CodeBlog.API.Repositories.Implementation;
+using CodeBlog.API.Repositories.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CodeBlog.API.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IBlogPostRepository, BlogPostRepository>();
+            services.AddScoped<IImageRepository, ImageRepository>();
+            services.AddScoped<IJwtTokenRepository, JwtTokenRepository>();
+        }
+
+        public static void RegisterAutoMapperProfiles(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(CategoryProfile));
+            services.AddAutoMapper(typeof(BlogPostProfile));
+        }
+        public static void AddDbContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CodeBlogConnectionString"));
+            });
+
+            services.AddDbContext<AuthDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CodeBlogConnectionString"));
+            });
+        }
+
+        public static void PasswordConfigurations(this IServiceCollection services)
+        {
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 1;
+            });
+        }
+
+        public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            AuthenticationType = "Jwt",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = configuration["Jwt:Issuer"],
+            ValidAudience = configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+        };
+    });
+        }
+    }
+}
--- CodeBlog.API/Program.cs
+++ CodeBlog.API/Program.cs
@@ -1,13 +1,7 @@
-using CodeBlog.API.Contracts.Profiles;
 using CodeBlog.API.Data;
-using CodeBlog.API.Repositories.Implementation;
-using CodeBlog.API.Repositories.Interface;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using CodeBlog.API.Extensions;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,53 +12,21 @@ builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("CodeBlogConnectionString"));
-});
 
-builder.Services.AddDbContext<AuthDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("CodeBlogConnectionString"));
-});
+builder.Services.AddDbContexts(builder.Configuration);
 
-builder.Services.AddAutoMapper(typeof(CategoryProfile));
-builder.Services.AddAutoMapper(typeof(BlogPostProfile));
+builder.Services.RegisterAutoMapperProfiles();
+builder.Services.RegisterServices();
 
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
-builder.Services.AddScoped<IImageRepository, ImageRepository>();
 builder.Services.AddIdentityCore<IdentityUser>()
     .AddRoles<IdentityRole>()
     .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("CodeBlogs")
     .AddEntityFrameworkStores<AuthDbContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 8;
-    options.Password.RequiredUniqueChars = 1;
-});
+builder.Services.PasswordConfigurations();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            AuthenticationType = "Jwt",
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-        };
-    });
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
